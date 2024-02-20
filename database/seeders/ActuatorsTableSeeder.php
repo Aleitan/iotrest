9c38ad7f7db585bb7620a61634fd0cdf837a02de
Aleitan <142346059+Aleitan@users.noreply.github.com>
2024-02-20 09:15:24 -0600

    first commit

---
+++ database/seeders/ActuatorsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Actuator;
+use Faker\Factory as Faker;
+
+class ActuatorsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+        for ($i = 0; $i < 20; $i++){
+            Actuator::create([
+                'name' => $faker->unique()->name(),
+                'type' => $faker->randomElement(['Led', 'Motor', 'Rele ']),
+                'value' => $faker->randomFloat(2, 0, 100),
+                'user_id' => rand(1, 11)
+            ]);
+        }
+    }
+}
